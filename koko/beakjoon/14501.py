9ca8cd64f3d2fc7fe4d98c07f44c3c9e344a2861
Koko <koko@kasa.sg>
2024-07-03 19:50:38 +0900

    [240703]백준 14501

---
+++ koko/beakjoon/14501.py
@@ -0,0 +1,23 @@
+# i = 근무가능일수
+# dp[i]  = 최대수익
+
+import sys
+
+n = int(input())
+
+input_schedule_and_profit = []
+
+for i in range(0, n):
+    t, p = map(int, sys.stdin.readline().rstrip().split())
+    input_schedule_and_profit.append((t, p))
+
+
+dp_table = [0] * (n+1)
+
+for i in range(0, n-1):
+    if i < input_schedule_and_profit[i][0]:
+        dp_table[i] = dp_table[i-1]
+    else:
+        dp_table[i] = max(dp_table[i-1], dp_table[i - input_schedule_and_profit[i][0]] + input_schedule_and_profit[i][1])
+
+print(dp_table[n])
