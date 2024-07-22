13091c7770bef8afa3ca7a0f99aea62a87a1859e
Lee-YunChang <dbsckd512@naver.com>
2024-07-22 22:59:21 +0900

    [mark] 0722 weekly study

--- mark/reetcode/91.py
+++ mark/reetcode/91.py
@@ -1,45 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        s_one_split_list = []
-        s_two_split_list = []
-        s_tow_split_list_back = []
+        if s[0] == '0':
+            return 0
 
-        if len(s) >= 2:
-            s_one_split_list = self.split_by_one(s)
-            s_two_split_list = self.split_by_two(s)
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[0] =1
+        dp[1] = 1 if s[0] != '0' else 0
 
-        if len(s) % 2 == 1:
-            s_tow_split_list_back = self.split_by_two_reverse(s)
+        for i in range(2, n + 1):
+            if (s[i - 1] != '0'):
+                dp[i] += dp[i - 1]
 
-        aList = [str(i) for i in range(1,27)]
+            nowNum = int(s[i - 2: i])
+            if (10 <= nowNum <= 26):
+                dp[i] += dp[i - 2]
 
-        cnt = 0
-
-        if len(s_one_split_list) != 0:
-            if all(s_one in aList for s_one in s_one_split_list):
-                cnt += 1
-        if len(s_two_split_list) != 0:
-            if all(s_two in aList for s_two in s_two_split_list):
-                cnt += 1
-
-        if len(s_tow_split_list_back) != 0:
-            if all(s_two_reverse in aList for s_two_reverse in s_tow_split_list_back):
-                cnt += 1
-
-        return cnt
-
-
-    def split_by_one(self, s):
-        return [s[i:i + 1] for i in range(0, len(s), 1)]
-    def split_by_two(self, s):
-        return [s[i:i + 2] for i in range(0, len(s), 2)]
-
-    def split_by_two_reverse(self, s):
-        reversed_s = s[::-1]
-        split_reversed = [reversed_s[i:i + 2] for i in range(0, len(reversed_s), 2)]
-        result = [chunk[::-1] for chunk in split_reversed]
-        return result[::-1]
+        return dp[n]
 
 
 print(Solution().numDecodings("2106"))
--- mark/reetcode/94.py
+++ mark/reetcode/94.py
@@ -3,28 +3,16 @@ from typing import List
 
 class Solution:
     def inorderTraversal(self, root) -> List[int]:
+        output = []
 
-        nodes = []
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                output.append(node.val)
+                dfs(node.right)
 
-        # 각 루트 값에 대해 TreeNode 객체를 생성하여 리스트에 추가
-        for i in range(len(root)):
-            nodes.append(TreeNode(root[i]))
-
-        print(nodes)
-
-        return nodes
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-class BinaryTree:
-    def __init__(self):
-        self.root = None
-
-
+        dfs(root)
+        return  output
 
 root = [1, None, 2, 3]
-Solution().inorderTraversal(root=root)
+Solution().inorderTraversal()
